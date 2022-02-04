b43c555268d15df449a935904708a7ee819a9a1c
jak010 <bluetoon@naver.com>
2022-02-04 17:57:33 +0900

    feat: django-filters CharFilter에 MultipleValue 적용

--- person/views/person.py
+++ person/views/person.py
@@ -1,5 +1,8 @@
+import http
+
 from django.db import IntegrityError
-import django_filters
+
+from django_filters.rest_framework import filters, filterset
 
 from rest_framework.generics import ListCreateAPIView
 from ..serializer import PersonSerializer
@@ -7,17 +10,21 @@ from ..exception import DuplicateNameError
 
 from ..models import Person
 
+from typing import List
 
-# XXX: Filter에 관한 노트
-## 만약 `CharFilter`로 다중으로 입력받고 싶다면 어떻게 해야될까?
-class PersonFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name='name')
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpRequest
+from django.db.models import Q
 
-    age = django_filters.NumberFilter(field_name='age', lookup_expr='exact')
-    age_gt = django_filters.NumberFilter(field_name='age', lookup_expr='gte')
-    age_lt = django_filters.NumberFilter(field_name='age', lookup_expr='lte')
 
-    sort = django_filters.OrderingFilter(
+class PersonFilter(filterset.FilterSet):
+    name = filters.CharFilter(field_name='name', method='filter_by_name')
+
+    age = filters.NumberFilter(field_name='age', lookup_expr='exact')
+    age_gt = filters.NumberFilter(field_name='age', lookup_expr='gte')
+    age_lt = filters.NumberFilter(field_name='age', lookup_expr='lte')
+
+    sort = filters.OrderingFilter(
         fields=(
             ('name', 'name'),
             ('age', 'age'),
@@ -27,6 +34,19 @@ class PersonFilter(django_filters.FilterSet):
     class Meta:
         model = Person
         fields = ['name', 'age']
+
+    # 만약 `CharFilter`로 MultipleValue로 입력받고 싶다면 어떻게 해야될까?
+    def filter_by_name(self, queryset, key: HttpRequest, value):
+        q = Q()
+
+        argument = self.request.GET.copy()
+        names: List = argument.getlist(key)
+
+        if len(names) != 1:
+            [q.add(Q(name=value), conn_type=q.OR)
+             for value in names],
+            return queryset.filter(q)
+        return queryset.filter(name=value)
 
 
 class PersonListCreateAPIView(ListCreateAPIView):
